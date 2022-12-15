c3acbf9e36230c76f928551eb412d3a4e57b04d9
AngelaPogoda <13811@coderacademy.edu.au>
2022-12-15 20:05:34 +1000

    functions, variables, readme.md info and comments made

--- hangman.py
+++ hangman.py
@@ -1,8 +1,18 @@
 # Initialisation
 game_over = False #initialise game as continuing
-
+guesses = [] # Array for visual of word being guessed
+incorrect_guesses = 0 
+remaining_guesses = 0 #update to number of hangman images
+game_words = words.game_words # game words from word file
 
 # print function
+def remaining_visuals():
+    return
+
+# game
+while game_over == False:
+    remaining_visuals()
+
 
 # end conditions
     # Lose
